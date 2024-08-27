4539de96ca149455f9efa79dbc0e53dee211621f
Алексей С. <76011034+sem-a@users.noreply.github.com>
2024-08-27 21:19:13 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,31 +1,92 @@
 const darkModeIsActive = () => {
     const flag = document.querySelector("#darkmodeButton");
     const body = document.querySelector("body");
+    const text = document.querySelector(".darkmode-text");
 
     if (flag.checked) {
-        console.log('убран')
         body.classList.remove("darkmode");
+        text.innerHTML = "Нужна темная тема?";
     } else {
         body.classList.add("darkmode");
+        text.innerHTML = "Нужна светлая тема?";
     }
 };
 
-const darkmodeButton = document.querySelector('#darkmodeButtonContainer');
+const createPortfolioCard = (data) => {
+    const cardsContainer = document.querySelector('.portfolio__cards');
 
-darkmodeButton.addEventListener('click', darkModeIsActive);
+    const portfolioCard = document.createElement('div');
+    portfolioCard.className = 'portfolio-card';
+
+    const cardContainer = document.createElement('div');
+    cardContainer.className = 'card-container';
+
+    const cardTitle = document.createElement('div');
+    cardTitle.className = 'card-title';
+
+    const cardLogo = document.createElement('div');
+    cardLogo.className = 'card-logo';
+    const logoImg = document.createElement('img');
+    logoImg.src = data.imgPath;
+    cardLogo.appendChild(logoImg);
+
+    const cardTitleText = document.createElement('h3');
+    cardTitleText.textContent = data.title;
+    cardTitle.appendChild(cardLogo);
+    cardTitle.appendChild(cardTitleText);
+
+    const cardDescription = document.createElement('div');
+    cardDescription.className = 'card-description';
+    const descriptionText = document.createElement('p');
+    descriptionText.className = 'text';
+    descriptionText.textContent = data.description;
+    cardDescription.appendChild(descriptionText);
+
+    const cardLink = document.createElement('div');
+    cardLink.className = 'card-link';
+    const linkText = document.createElement('p');
+    linkText.className = 'text';
+    linkText.textContent = 'перейти';
+    const arrowDiv = document.createElement('div');
+    arrowDiv.className = 'arrow';
+    cardLink.appendChild(linkText);
+    cardLink.appendChild(arrowDiv);
+
+    // Добавление элементов в контейнер
+    cardContainer.appendChild(cardTitle);
+    cardContainer.appendChild(cardDescription);
+    cardContainer.appendChild(cardLink);
+    portfolioCard.appendChild(cardContainer);
+
+    // Добавление карточки в тело документа
+    cardsContainer.appendChild(portfolioCard);
+    console.log(data)
+}
+
+const data = fetch('../data/portfolio.json')
+    .then((response) => response.json())
+    .then((json) => {
+        for (let i = 0; i < json.portfolio.length; i++) {
+            createPortfolioCard(json.portfolio[i]);
+        }
+    });
+
+const darkmodeButton = document.querySelector("#darkmodeButtonContainer");
+
+darkmodeButton.addEventListener("click", darkModeIsActive);
 
 // плавная прокрутка до якоря
-const anchors = document.querySelectorAll('a[href*="#"]')
+const anchors = document.querySelectorAll('a[href*="#"]');
 
 for (let anchor of anchors) {
-  anchor.addEventListener('click', function (e) {
-    e.preventDefault()
-    
-    const blockID = anchor.getAttribute('href').substr(1)
-    
-    document.getElementById(blockID).scrollIntoView({
-      behavior: 'smooth',
-      block: 'start'
-    })
-  })
+    anchor.addEventListener("click", function (e) {
+        e.preventDefault();
+
+        const blockID = anchor.getAttribute("href").substr(1);
+
+        document.getElementById(blockID).scrollIntoView({
+            behavior: "smooth",
+            block: "start",
+        });
+    });
 }
